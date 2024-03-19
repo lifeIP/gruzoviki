33e8c6178fdf46b2454ae00d36ea9196e7ca0e36
lifeIP <molokeevartem@gmail.com>
2024-03-19 17:06:14 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Cookies, useCookies } from 'react-cookie';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Button from '@mui/material/Button';
+import MyHeader from './components/MyHeader';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(['theme']);
+  //const [theme, setTheme] = useState(darkTheme);
+
+  const [selectedTheme, setSelectedTheme] = useState(()=>{
+    if(cookies.theme === undefined){
+      setCookie('theme', true, { path: '/' });  
+      return lightTheme;
+    }
+    else{
+      if(cookies.theme) return lightTheme; 
+      else return darkTheme;
+    }
+  });
+
+  function changeTheme(){
+    if(selectedTheme == lightTheme) setSelectedTheme(darkTheme);
+    else setSelectedTheme(lightTheme);
+    setCookie('theme', selectedTheme == lightTheme ? false : true, { path: '/' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={selectedTheme}>
+      <MyHeader selectedTheme={selectedTheme == lightTheme ? true:false} changeTheme={changeTheme}/>
+      <div className="App">
+        <Button  
+          onClick={() => {
+            changeTheme();
+            console.log(cookies.theme);
+          }}
+          variant="chtheme"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Change theme
+        </Button>
+        <Button  
+        onClick={() => {
+          console.log(cookies.theme);
+        }}
+        variant="chtheme">Change theme</Button>
+
+      </div>
+      <CssBaseline />
+      
+    </ThemeProvider>
   );
 }
 
